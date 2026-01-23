3aa8f069b45780241910c029e561bddebb41d0b5
mariembenftima <mariembenftima@gmail.com>
2026-01-23 22:38:24 +0100

    admin

--- leave_management_system_front/src/app/private/components/admin/users/services/users.service.ts
+++ leave_management_system_front/src/app/private/components/admin/users/services/users.service.ts
@@ -2,9 +2,10 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { environment } from '../../../../../../environments/environment.development';
 import { User } from '../../../../../types/user.model';
 import { CreateEmployeeProfileDto } from '../users/modals/create-profile-modal/create-profile-modal.component';
+import { environment } from '../../../../../../environments/environment';
+
 
 
 export interface PaginationData {
